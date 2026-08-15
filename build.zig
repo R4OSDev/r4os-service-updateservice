41303e3f13f6d2ec6e4ca05cba3ecd40e23e22ae
R4mboX <R4mboX@users.noreply.github.com>
2026-08-15 21:08:33 +0200

    SDK- und Library-Abhaengigkeiten paketfest aufloesen

--- build.zig
+++ build.zig
@@ -3,42 +3,46 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const sdk_build = b.lazyImport(@This(), "r4os_sdk") orelse return;
     const sdk_dep = b.dependencyFromBuildZig(sdk_build, .{});
+    const libraries_build = b.lazyImport(@This(), "r4os_libraries") orelse return;
+    const libraries_dep = b.dependencyFromBuildZig(libraries_build, .{});
     const sdk = sdk_build.sdk(b, sdk_dep, .{});
-    _ = sdk.addR4MF(b.path("module.R4MF"));
-
-    const host_r4os = b.createModule(.{
-        .root_source_file = sdk_dep.path("r4os.zig"),
-        .target = b.graph.host,
-        .optimize = .Debug,
+    _ = sdk.addR4MFWithOptions(b.path("module.R4MF"), .{
+        .zig_module_roots = &.{
+            sdk_dep.namedLazyPath("system_update_engine"),
+            sdk_dep.namedLazyPath("update_service_contract"),
+            libraries_dep.namedLazyPath("r4std_zig_binding"),
+        },
     });
+
+    const host_r4os = sdk.createR4osModule(b.graph.host, .Debug);
     const r4std_abi = b.createModule(.{
-        .root_source_file = b.path("../../Libraries/R4STD/Bindings/Zig/r4std_abi.zig"),
+        .root_source_file = libraries_dep.path("R4STD/Bindings/Zig/r4std_abi.zig"),
         .target = b.graph.host,
         .optimize = .Debug,
     });
     r4std_abi.addImport("r4os", host_r4os);
     const r4std = b.createModule(.{
-        .root_source_file = b.path("../../Libraries/R4STD/Bindings/Zig/r4std.zig"),
+        .root_source_file = libraries_dep.path("R4STD/Bindings/Zig/r4std.zig"),
         .target = b.graph.host,
         .optimize = .Debug,
     });
     r4std.addImport("r4os", host_r4os);
     r4std.addImport("r4std_abi.zig", r4std_abi);
     const implementation = b.createModule(.{
-        .root_source_file = b.path("../../Libraries/R4STD/Contract/Generated/implementation_abi.zig"),
+        .root_source_file = libraries_dep.path("R4STD/Contract/Generated/implementation_abi.zig"),
         .target = b.graph.host,
         .optimize = .Debug,
     });
     implementation.addImport("r4os", host_r4os);
     const provider = b.createModule(.{
-        .root_source_file = b.path("../../Libraries/R4STD/Source/main.zig"),
+        .root_source_file = libraries_dep.path("R4STD/Source/main.zig"),
         .target = b.graph.host,
         .optimize = .Debug,
     });
     provider.addImport("r4os", host_r4os);
     provider.addImport("r4l_contract", implementation);
     const r4std_test = b.createModule(.{
-        .root_source_file = b.path("../../Libraries/R4STD/Tests/consumer_runtime.zig"),
+        .root_source_file = libraries_dep.path("R4STD/Tests/consumer_runtime.zig"),
         .target = b.graph.host,
         .optimize = .Debug,
     });
